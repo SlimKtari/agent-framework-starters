a3d3352a8124d932191274d1726934d4b7a3c138
Slim Ktari <code@slimktari.com>
2026-02-09 00:19:52 +0100

    feat(HelloAgent): add simple agent

---
+++ AgentFramework.Starters/AgentFramework.HelloAgent/Extensions/AiPlatformExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Agents.AI.DevUI;
+
+namespace AgentFramework.HelloAgent.Extensions;
+public static class AiPlatformExtensions
+{
+    public static WebApplicationBuilder AddAiPlatform(this WebApplicationBuilder builder)
+    {
+        builder.AddOpenAIResponses();
+        builder.AddOpenAIChatCompletions();
+        builder.Services.AddOpenAIConversations();
+        builder.AddDevUI();
+        return builder;
+    }
+
+    public static WebApplication MapAiPlatform(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapDevUI();
+        }
+        app.MapOpenAIResponses();
+        app.MapOpenAIConversations();
+        return app;
+    }
+}
